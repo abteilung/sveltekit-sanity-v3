92f16d4c72cce258f3aef398efb7e2368f37a5c6
Pascal <pascal.widmer@abteilung.ch>
2023-03-12 20:53:49 +0100

    xx

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,11 +1,10 @@
-import type {LayoutServerLoad} from './$types'
-import {getSanityServerClient} from '../../lib/config/sanity/client'
-import {getSiteConfig, getMenus} from '../../lib/config/sanity/queries'
+import {getSanityServerClient} from '$lib/config/sanity/client'
+import {getSiteConfig, getMenus} from '$lib/config/sanity/queries'
 
 // Types
 /** @type {import('@sveltejs/kit').RequestHandler} */
 
-export const load: LayoutServerLoad = async ({locals: {previewMode}, url}) => {
+export const load = async ({locals: {previewMode}, url}) => {
   const siteConfig = await getSanityServerClient(false).fetch(getSiteConfig)
   const menus = await getSanityServerClient(false).fetch(getMenus)
 
