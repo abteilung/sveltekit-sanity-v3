3a7c160fe40ffd6aac4ebdae53239846aee25dd8
Pascal <pascal.widmer@abteilung.ch>
2023-03-28 10:31:25 +0200

    x

--- src/lib/Components/PT/Image.svelte
+++ src/lib/Components/PT/Image.svelte
@@ -1,27 +1,17 @@
 <script lang="ts">
   import {urlForImage} from '$lib/config/sanity'
-
+  
   export let portableText: any;
-  $: ({value} = portableText)
   
   export let width: number = 940
   export let height: number = 480
-  export let alt: string = 'Image'
+  export let alt: string = portableText.value.alt
   export let src: string = portableText.value.asset
-  // portableText.value.asset.metadata.lqip
   export let lqip: string = portableText.value.lqip
-  // portableText.value.asset.metadata.palette.dominant.background
   export let bgColor: string = portableText.value.bgColor
   export let customRatio: number = portableText.value.customRatio
-
 </script>
 
-<div style="background-image: url({lqip}); background-color: {bgColor};">
+<div class="w-full" style="background-image: url({lqip}); background-color: {bgColor};">
   <img src={urlForImage(src, width, customRatio ? Math.round(width / customRatio) : height)} {alt} />
 </div>
-
-<style lang="postcss">
-  img {
-    @apply w-full;
-  }
-</style>
--- src/routes/(app)/[slug]/+page.svelte
+++ src/routes/(app)/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import {urlForImage} from '$lib/config/sanity'
   import type {PageData} from './$types'
+  import PortableBlock from '$lib/Components/PortableBlock.svelte'
 
   export let data: PageData
 
@@ -11,4 +12,9 @@
   <title>Layout with left Navigation and revealing Footer</title>
 </svelte:head>
 
-{page.title}
+<h4>{page.subtitle}</h4>
+<h1>{page.title}</h1>
+
+{#if page.body}
+  <PortableBlock content={page.body} />
+{/if}
