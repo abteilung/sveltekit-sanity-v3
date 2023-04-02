5f5bdab86d1a55cbf2c14c0ba9565d8e825a396a
Pascal <pascal.widmer@abteilung.ch>
2023-04-02 18:33:26 +0200

    removeStoreForHeaderimage

--- src/lib/Stores/Stores.ts
+++ src/lib/Stores/Stores.ts
@@ -1,7 +1,4 @@
 import {writable, type Writable} from 'svelte/store'
 
-// Store for Header Image. Experimental feature, does not work nicely yet.
-export const HeaderImage: Writable<string> = writable('')
-
 // Store for Cookie Banner
 export const cookieVisibility = writable(false)
