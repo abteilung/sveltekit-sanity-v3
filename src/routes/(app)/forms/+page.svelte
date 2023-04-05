068ead3ac32846bf50770f0900802afec6c0759f
Pascal <pascal.widmer@abteilung.ch>
2023-04-05 19:37:38 +0200

    addValidators.BasicOnes

--- src/routes/(app)/forms/+page.svelte
+++ src/routes/(app)/forms/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
   import {page} from '$app/stores'
+  import classNames from 'classnames'
+
   import {superForm} from 'sveltekit-superforms/client'
   import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte'
 
   export let data
 
-  const {form, errors, constraints, message, enhance} = superForm(data.form)
+  const {form, errors, constraints, message, enhance} = superForm(data.form, {
+    validators: {
+      first_name: (value) => value.length < 1 ? 'Wie ist Ihr Vorname?' : null,
+      last_name: (value) => value.length < 1 ? 'Wir wüssten gern Ihren Nachnamen!' : null,
+      message: (value) => value.length < 1 ? 'Wollten Sie uns nicht etwas mitteilen?' : null,
+    },
+  })
 </script>
 
 <SuperDebug data={$form} />
@@ -17,36 +25,43 @@
   </div>
 {/if}
 
-<form method="POST" use:enhance>
-  <div>
-    <input class={$errors.first_name ? 'invalid' : ''} type="text" placeholder="Vorname" name="first_name" data-invalid={$errors.first_name} bind:value={$form.first_name} />
+<form method="POST" use:enhance class="space-y-4">
+  <div class="relative">
+    <input class={classNames('w-full border-light bg-light', $errors.first_name ? 'invalidField' : '')} type="text" placeholder="Vorname" name="first_name" data-invalid={$errors.first_name} bind:value={$form.first_name} />
     {#if $errors.first_name}<div class="invalid">{$errors.first_name}</div>{/if}
   </div>
-  <div>
-    <input class={$errors.last_name ? 'invalid' : ''} type="text" placeholder="Nachname" name="last_name" data-invalid={$errors.last_name} bind:value={$form.last_name} />
+  <div class="relative">
+    <input class={classNames('w-full border-light bg-light', $errors.last_name ? 'invalidField' : '')} type="text" placeholder="Nachname" name="last_name" data-invalid={$errors.last_name} bind:value={$form.last_name} />
     {#if $errors.last_name}<div class="invalid">{$errors.last_name}</div>{/if}
   </div>
-  <div>
-    <input class={$errors.email ? 'invalid' : ''} type="email" placeholder="E-Mail" name="email" data-invalid={$errors.email} bind:value={$form.email} />
+  <div class="relative">
+    <input class={classNames('w-full border-light bg-light', $errors.email ? 'invalidField' : '')} type="email" placeholder="E-Mail" name="email" data-invalid={$errors.email} bind:value={$form.email} />
     {#if $errors.email}<div class="invalid">{$errors.email}</div>{/if}
   </div>
-  <div>
-    <input class={$errors.phone ? 'invalid' : ''} type="text" placeholder="Telefonnummer" name="phone" data-invalid={$errors.phone} bind:value={$form.phone} />
+  <div class="relative">
+    <input class={classNames('w-full border-light bg-light', $errors.phone ? 'invalidField' : '')} type="text" placeholder="Telefonnummer" name="phone" data-invalid={$errors.phone} bind:value={$form.phone} />
     {#if $errors.phone}<div class="invalid">{$errors.phone}</div>{/if}
   </div>
-  <div>
-    <textarea class={$errors.message ? 'invalid' : ''} type="text" placeholder="Ihre Mitteilung" name="message" data-invalid={$errors.message} bind:value={$form.message} />
+  <div class="relative">
+    <textarea class={classNames('w-full border-light bg-light', $errors.message ? 'invalidField' : '')} type="text" placeholder="Ihre Mitteilung" name="message" data-invalid={$errors.message} bind:value={$form.message} />
     {#if $errors.message}<div class="invalid">{$errors.message}</div>{/if}
   </div>
-  <div>
+  <div class="relative">
     <button>Abschicken</button>
   </div>
 </form>
 
 <style lang="postcss">
+  input {
+    @apply focus:border-transparent focus:ring-0;
+  }
   .invalid {
-    @apply text-primary text-sm;
-    @apply border-accent;
+    @apply absolute top-0 right-0;
+    @apply bg-primary text-sm text-white px-2;
+  }
+
+  .invalidField {
+    @apply border-b-2 border-b-primary !important;
   }
   .valid {
     @apply border-success;
