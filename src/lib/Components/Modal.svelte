7e283eb7e05bb3f0fa0272dc59d5e8502f5a7af4
Pascal <pascal.widmer@abteilung.ch>
2023-04-11 17:04:13 +0200

    addModal

--- src/lib/Components/Modal.svelte
+++ src/lib/Components/Modal.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-  import {browser} from '$app/environment'
+  import {browser} from '$app/environment';
+  import { fade, fly } from 'svelte/transition'
+  import { cubicInOut } from 'svelte/easing'
 
-  export let content = ''
+  export let content = 'I am a Modal'
   let isModalOpen: boolean = false
 
   // Function to toggle State of Modal
@@ -24,10 +26,12 @@
   $: if (browser && isModalOpen) {
     document.body.classList.add('modal-open')
   } else if (browser && !isModalOpen) {
-    document.body.classList.remove('modal-open')
+    // Add Delay
+    setTimeout(() => {
+      document.body.classList.remove('modal-open')
+    }, 350)
   }
 
-  $: ({content} = content)
 </script>
 
 <svelte:window on:keydown={handleKeyDown} />
@@ -35,9 +39,20 @@
 <button class="bg-black text-white px-4 py-2" on:click={openModal}> Open Da Modal </button>
 
 {#if isModalOpen}
-  <div class="bg-black/80 fixed justify-center items-center inset-0 w-full h-full z-50" on:click={closeModal}>
-    <div class="container bg-white" on:click|stopPropagation>
-      Content: {content}
+  <div 
+    in:fade={{duration: 150, easing: cubicInOut}}
+    out:fade={{duration: 150, easing: cubicInOut, delay: 300}}
+    class="bg-black/80 fixed justify-center items-center inset-0 w-full h-full z-50" 
+    on:click={closeModal}
+    >
+    <div 
+        in:fly={{y: -150, duration: 250, easing: cubicInOut, delay: 150}}
+        out:fly={{y: 150, duration: 250, easing: cubicInOut, delay: 150}}
+        class="h-full w-full flex justify-center items-center"
+    >
+        <div class="w-1/2 p-12 h-96 bg-white" on:click|stopPropagation>
+            {content}
+        </div>
     </div>
   </div>
 {/if}
