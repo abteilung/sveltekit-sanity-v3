81c28da32d99e674949b0cb7fe0e2c4f4a440910
Pascal <pascal.widmer@abteilung.ch>
2023-04-17 16:01:54 +0200

    upgradePackages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -40,7 +40,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
     require('tailwindcss-hyphens'),
     // If using VidStack
     require('vidstack/tailwind.cjs')
