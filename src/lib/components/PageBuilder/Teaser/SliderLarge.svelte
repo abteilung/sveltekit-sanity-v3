79749badefc8b5a99ebf9f018d7d924b64ac0b10
Pascal <pascal.widmer@abteilung.ch>
2023-05-04 11:36:52 +0200

    tryingToFixIntegrationOfSliderLarge

--- src/lib/components/PageBuilder/Teaser/SliderLarge.svelte
+++ src/lib/components/PageBuilder/Teaser/SliderLarge.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import {fly} from 'svelte/transition'
   import SwiperCore, {Controller, EffectCreative, Pagination} from 'swiper'
   import {Swiper, SwiperSlide} from 'swiper/svelte'
   import 'swiper/css'
