0da10fbf043785f5a2a72798f6f637e2f58e7eec
Pascal <pascal.widmer@abteilung.ch>
2023-05-16 10:18:36 +0200

    x

--- src/lib/Stores/stores.ts
+++
@@ -1,4 +0,0 @@
-import {writable, type Writable} from 'svelte/store'
-
-// Store for Cookie Banner
-export const cookieVisibility = writable(false)
--- src/lib/Stores/theme.ts
+++
@@ -1,4 +0,0 @@
-import {writable} from 'svelte/store'
-import type {Theme} from '../../hooks.server'
-
-export const theme = writable<Theme>()
